f1994d0bed9660bc3fbc360e8837f62568bfcf82
Rayhan-50 <rayhanahmed.nstu@gmail.com>
2025-12-10 17:46:35 +0600

    fix

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -134,7 +134,8 @@ import {
     updateProfile
 } from 'firebase/auth';
 import { app } from '../firebase/firebase.config';
-import useAxiosPublic from '../hooks/UseAxiosPublic';
+
+import useAxiosPublic from '@/hooks/useAxiosPublic';
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
